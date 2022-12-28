233ff3b62e8118f460115ad68d309a7f791aa7ca
katsu8 <katsumi.ishihara08@gmail.com>
2022-12-28 17:31:55 +0100

    change logo img import route

--- components/SkillList/index.tsx
+++ components/SkillList/index.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import Skill from "./Skill";
-import HtmlImg from "../public/assets/skills/html.png";
-import CssImg from "../public/assets/skills/css.png";
-import JSImg from "../public/assets/skills/javascript.png";
-import TSImg from "../public/assets/skills/typescript.png";
-import ReactImg from "../public/assets/skills/react.png";
-import NextJSImg from "../public/assets/skills/nextjs.png";
-import TailwindImg from "../public/assets/skills/tailwind.png";
-import NodeImg from "../public/assets/skills/node.png";
-import GraphQLImg from "../public/assets/skills/graphql.png";
-import PrismaImg from "../public/assets/skills/prisma.png";
-import PostgreSQLImg from "../public/assets/skills/postgresql.png";
+import HtmlImg from "../../public/assets/skills/html.png";
+import CssImg from "../../public/assets/skills/css.png";
+import JSImg from "../../public/assets/skills/javascript.png";
+import TSImg from "../../public/assets/skills/typescript.png";
+import ReactImg from "../../public/assets/skills/react.png";
+import NextJSImg from "../../public/assets/skills/nextjs.png";
+import TailwindImg from "../../public/assets/skills/tailwind.png";
+import NodeImg from "../../public/assets/skills/node.png";
+import GraphQLImg from "../../public/assets/skills/graphql.png";
+import PrismaImg from "../../public/assets/skills/prisma.png";
+import PostgreSQLImg from "../../public/assets/skills/postgresql.png";
 
 const SkillList = () => {
   return (
